f2e0edd50588897a743b6fa80f03622242cd3462
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-05-17 18:57:20 -0300

    build: create project

---
+++ src/main/java/code/blurone/reusableshriekers/ReusableShriekers.kt
@@ -0,0 +1,13 @@
+package code.blurone.reusableshriekers
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ReusableShriekers : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
